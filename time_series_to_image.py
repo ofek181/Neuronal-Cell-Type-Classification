483e4d2242d7837070a5cdcb455ed722480c1b55
Ofek Ophir <ophir.ofek@gmail.com>
2022-06-17 16:55:30 +0300

    added docstring

--- time_series_to_image.py
+++ time_series_to_image.py
@@ -5,6 +5,10 @@ IMG_SIZE = 224
 
 
 def activity_to_image_gaf(activity: np.array) -> np.ndarray:
+    """
+    :param activity: time series response for the sweep data of the cell.
+    :return: gramian angular field image of the activity time series.
+    """
     gaf = GramianAngularField(image_size=IMG_SIZE, method='d')
     image = np.empty((IMG_SIZE, IMG_SIZE, 3))
     segment_length = int(len(activity)/3)
