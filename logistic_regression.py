97a1793388f1ee90afbfc8cb9563e823a4bf30b7
Ofek Ophir <ophir.ofek@gmail.com>
2022-06-19 00:32:00 +0300

    added all the abs functions implementations

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import StratifiedKFold
 from sklearn.preprocessing import StandardScaler
@@ -8,7 +9,10 @@ from helper_functions import calculate_metrics
 
 
 class LogisticClassifier(Model):
-    def __init__(self, db: pd.DataFrame):
+    def __init__(self, db: pd.DataFrame) -> None:
+        """
+        :param db: cell ephys features dataframe.
+        """
         db = db.dropna(axis=1, how='all')
         db = db.dropna(axis=0)
         irrelevant_columns = ['layer', 'structure_area_abbrev', 'sampling_rate', 'mean_clipped', 'file_name']
@@ -16,17 +20,22 @@ class LogisticClassifier(Model):
         super(LogisticClassifier, self).__init__(db)
 
     def _create_model(self) -> LogisticRegression:
+        """
+        :return: creates a logistic regression classifier
+        """
         model = LogisticRegression()
         return model
 
-    def train_and_test(self):
+    def train_and_test(self) -> pd.DataFrame:
+        """
+        :return: results of the logistic regression classifier on the testing data.
+        """
         df = self._data
         df['dendrite_type'] = pd.Categorical(df['dendrite_type'])
         df['dendrite_type'] = df['dendrite_type'].cat.codes
         y = df.pop('dendrite_type')
         y = y.values.astype(float)
         x = df.values
-
         kf = StratifiedKFold(n_splits=3)
         stats = []
         for train_index, test_index in kf.split(x, y):
@@ -38,29 +47,38 @@ class LogisticClassifier(Model):
             self.model.fit(x_train, y_train)
             y_pred = self.model.predict(x_test)
             accuracy, f1, precision, recall, roc_auc = calculate_metrics(y_test, y_pred)
-            results = [accuracy, f1, precision, recall, roc_auc]
-            stats.append(results)
-            print('Accuracy: {}, F1: {}, Precision: {}, Recall {}, ROC_AUC: {}'.format(
-                accuracy, f1, precision, recall, roc_auc))
+            stats.append([accuracy, f1, precision, recall, roc_auc])
+        results = pd.DataFrame(stats, columns=['Accuracy', 'F1 Score', 'Precision', 'Recall', 'ROC AUC'])
+        print('--------------------------------------------------------------')
         print("Logistic Regression Coefficients: " + str(self.model.coef_))
-
-        # sum_cm = np.asarray([x[2] for x in stats]).sum(axis=0)
-        # sum_cm = sum_cm / sum_cm.astype(np.float).sum(axis=1)
-        # params = {'a': None}
-        # res = {'mean_accuracy': mean_accuracy, 'mean_f1': mean_f1}
-        # self._save_results(params, res, sum_cm, 'nb')
+        print('--------------------------------------------------------------')
+        print("Mean Accuracy: " + str(results["Accuracy"].mean()))
+        print("Mean F1 Score: " + str(results["F1 Score"].mean()))
+        print("Mean Precision: " + str(results["Precision"].mean()))
+        print("Mean Recall: " + str(results["Recall"].mean()))
+        print("Mean ROC AUC: " + str(results["ROC AUC"].mean()))
+        return results
 
     @staticmethod
-    def save_results(results: pd.DataFrame, path: str, name: str):
-        pass
+    def save_results(results: pd.DataFrame, path: str, name: str) -> None:
+        """
+        :param results: results on the testing data.
+        :param path: path to save file.
+        :param name: name of the file.
+        :return: None.
+        """
+        results.to_csv(os.path.join(path, name))
 
 
 if __name__ == '__main__':
     dir_path = os.path.dirname(os.path.realpath(__file__))
     dataframe_path = dir_path + '\\data\\dataframe'
     dataframe_name = 'extracted_mean_ephys_data.csv'
-    df = pd.read_csv(dataframe_path + '\\' + dataframe_name)
-    lrclf = LogisticClassifier(df)
-    lrclf.train_and_test()
+    data = pd.read_csv(dataframe_path + '\\' + dataframe_name)
+    lrclf = LogisticClassifier(data)
+    lr_results = lrclf.train_and_test()
+    results_path = dir_path + '\\results\\logistic_regression'
+    model_name = 'logistic_regression.csv'
+    LogisticClassifier.save_results(lr_results, results_path, model_name)
 
 
