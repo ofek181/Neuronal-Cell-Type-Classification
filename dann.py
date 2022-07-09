ec34ca99a9c31a579675049e6dda5d991d759d7a
Ofek Ophir <ophir.ofek@gmail.com>
2022-07-09 13:15:26 +0300

    changed stop accuracy to 0.94

--- dann.py
+++ dann.py
@@ -27,7 +27,6 @@ if tf.test.gpu_device_name():
 else:
     print("No GPU found")
 
-# TODO test trained network on human data alone vs mouse data alone
 # TODO plot confusion matrix
 # TODO make the code pretty and add docstring
 # TODO find hyper-parameters that work well for the domain adaptation task
@@ -269,7 +268,8 @@ def main(args):
     batches = [16, 32, 64]
     epochs = [100, 250, 500, 1000, 2000]
     optimizers = ['adam', 'sgd', 'rmsprop']
-    lambdas = [0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 1, 1.1, 1.2, 1.3, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5]
+    lambdas = [0.3, 0.35, 0.4, 0.45, 0.48, 0.5, 0.52, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8,
+               1, 1.1, 1.2, 1.3, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5]
 
     DANN = DANNClassifier(db=data,
                           n_layers=1,
@@ -347,7 +347,7 @@ def main(args):
                                             n_run += 1
                                             results.to_csv(os.path.join(results_path, 'DANN_results.csv'), index=True)
 
-                                            if human_acc > 0.93 and mouse_acc > 0.93:
+                                            if human_acc > 0.94 and mouse_acc > 0.94:
                                                 print("hyper parameters found!")
                                                 print("Results are:")
                                                 print("=============================================================")
