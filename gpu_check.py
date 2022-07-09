8f2895b13e3feb15ceafa9b6d663b861ba71e4aa
Ofek Ophir <ophir.ofek@gmail.com>
2022-07-09 23:07:52 +0300

    updated test

--- gpu_check.py
+++ gpu_check.py
@@ -1,2 +1,8 @@
 import tensorflow as tf
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
+if tf.test.gpu_device_name():
+    print('Default GPU Device:.{}'.format(tf.test.gpu_device_name()))
+
