9a092be0efb5faa47e2e9d42066edb05d61b9138
Ofek Ophir <ophir.ofek@gmail.com>
2022-07-12 15:59:43 +0300

    updated to organism type

--- tsne.py
+++ tsne.py
@@ -9,10 +9,13 @@ from classifier import Model
 import warnings
 warnings.simplefilter("ignore")
 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
 
 class Tsne(Model):
     def __init__(self, db: pd.DataFrame) -> None:
         """
+        Initializes the model.
         :param db: cell ephys features dataframe.
         """
         # encoder = LabelEncoder()
@@ -21,53 +24,72 @@ class Tsne(Model):
         db = db.dropna(axis=0)
         irrelevant_columns = ['layer', 'structure_area_abbrev', 'sampling_rate', 'mean_clipped', 'file_name']
         db = db.drop([x for x in irrelevant_columns if x in db.columns], axis=1)
-        # db['dendrite_type'] = encoder.fit_transform(db['dendrite_type'])
-        db = db.drop(["dendrite_type"], axis=1)
         db = scaler.fit_transform(db)
         super(Tsne, self).__init__(db)
 
     def _create_model(self) -> TSNE:
         """
-        :return: creates a logistic regression classifier
+        :return: constructs the t-SNE algorithm.
         """
         model = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=300, init='pca')
         return model
 
     def train_and_test(self) -> np.ndarray:
         """
-        :return: results of the logistic regression classifier on the testing data.
+        :return: fit the t-SNE algorithm on the data
         """
         tsne_results = self.model.fit_transform(self.data)
         return tsne_results
 
     @staticmethod
-    def save_results() -> None:
+    def save_results(**kwargs) -> None:
         pass
 
-    def plot_tsne(self, results: np.ndarray, labels: pd.Series) -> None:
+    @staticmethod
+    def plot_tsne(results: np.ndarray, labels: pd.Series) -> None:
         """
-        :param results: results of the TSNE algorithm.
-        :param labels: true labels of the data.
-        :return: plot an image of the results.
+        :param results: results of the t-SNE dimensionality reductions.
+        :param labels: future hue for the plot.
+        :return: plot the first 2 dimensions of the t-SNE.
         """
-        tsne_res = pd.DataFrame()
-        tsne_res['tsne-1'] = results[:, 0]
-        tsne_res['tsne-2'] = results[:, 1]
-        tsne_res['dendrite_type'] = labels
+        res = pd.DataFrame()
+        res['tsne-1'] = results[:, 0]
+        res['tsne-2'] = results[:, 1]
+        res['organism'] = labels
         plt.figure(figsize=(16, 10))
-        sns.scatterplot(x="tsne-1", y="tsne-2", hue="dendrite_type", data=tsne_res, legend="full", alpha=0.3)
+        sns.scatterplot(x="tsne-1", y="tsne-2", hue="organism", data=res)
         plt.title("t-SNE plot of the extracted cell type features data")
         plt.show()
 
 
-if __name__ == '__main__':
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    dataframe_path = dir_path + '\\data\\dataframe'
-    dataframe_name = 'extracted_mean_ephys_data.csv'
-    data = pd.read_csv(dataframe_path + '\\' + dataframe_name)
+def get_data() -> pd.DataFrame:
+    """
+    :return: tuple of merged mouse/human data.
+    """
+    # get data from directories
+    dataframe_path_mouse = dir_path + '/data/dataframe/mouse'
+    dataframe_path_human = dir_path + '/data/dataframe/human'
+    name = 'extracted_mean_ephys_data.csv'
+    data_mouse = pd.read_csv(dataframe_path_mouse + '/' + name)
+    data_human = pd.read_csv(dataframe_path_human + '/' + name)
+    data_mouse['organism'] = 'mouse'
+    data_human['organism'] = 'human'
+    data = data_mouse.append(data_human, ignore_index=True)
+    return data
+
+
+def main():
+    data = get_data()
+    organism_type = data["organism"]
+    dendrite_type = data["dendrite_type"]
+    data = data.drop(["organism", "dendrite_type"], axis=1)
     tsne = Tsne(data)
     res = tsne.train_and_test()
-    tsne.plot_tsne(res, data["dendrite_type"])
+    tsne.plot_tsne(res, organism_type)
+
+
+if __name__ == '__main__':
+    main()
 
 
 
