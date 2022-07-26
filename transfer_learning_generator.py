cbb3219fd1cf73cf96a052c74b45bd1227d5e881
Ofek Ophir <ophir.ofek@gmail.com>
2022-07-26 21:17:27 +0300

    bugfix for new pathing

--- transfer_learning_generator.py
+++ transfer_learning_generator.py
@@ -1,15 +1,9 @@
-import pandas as pd
-import numpy as np
-import os
-import keras
-import matplotlib.pyplot as plt
 from keras.layers import Dense,GlobalAveragePooling2D, Dropout, Flatten
 from keras.applications import ResNet50
-from keras.preprocessing import image
 from keras.applications.resnet_v2 import preprocess_input
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Model
-from keras.optimizers import Adam
+
 
 BATCH_SIZE = 32
 DATA = 'gaf'
@@ -17,17 +11,18 @@ CLASSES = ['spiny', 'aspiny']
 
 n_classes = len(CLASSES)
 
-base_model = ResNet50(weights='imagenet',include_top=False)
+base_model = ResNet50(weights='imagenet', include_top=False)
 
 for layer in base_model.layers:
     layer.trainable = False
 
 x = base_model.output
 x = GlobalAveragePooling2D()(x)
-x = Dense(1024,activation='relu')(x)
-x = Dropout(0.5)(x)
-x = Dense(512,activation='relu')(x)
-x = Dropout(0.5)(x)
+x = Dense(512, activation='relu')(x)
+# x = Dropout(0.5)(x)
+x = Dense(256, activation='relu')(x)
+# x = Dropout(0.5)(x)
+x = Dense(128, activation='relu')(x)
 preds = Dense(n_classes, activation='softmax')(x)
 
 
@@ -35,16 +30,18 @@ model = Model(inputs=base_model.input, outputs=preds)
 
 
 train_datagen = ImageDataGenerator(preprocessing_function=preprocess_input, validation_split=0.2)
-train_generator = train_datagen.flow_from_directory('data/images/png',
-                                                    target_size=(224,224),
+
+train_generator = train_datagen.flow_from_directory('data/images/gaf_gray/png/mouse',
+                                                    target_size=(224, 224),
                                                     color_mode='rgb',
                                                     batch_size=BATCH_SIZE,
                                                     # class_mode='categorical',
                                                     classes=CLASSES,
                                                     shuffle=True,
                                                     subset='training')
-validation_generator = train_datagen.flow_from_directory('data/images/png',
-                                                         target_size=(224,224),
+
+validation_generator = train_datagen.flow_from_directory('data/images/gaf_gray/png/mouse',
+                                                         target_size=(224, 224),
                                                          color_mode='rgb',
                                                          batch_size=BATCH_SIZE,
                                                          # class_mode='categorical',
@@ -56,7 +53,8 @@ model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['accur
 
 
 step_size_train = train_generator.n//train_generator.batch_size
+
 model.fit_generator(generator=train_generator,
                     steps_per_epoch=step_size_train,
-                    epochs=10,validation_data = validation_generator,
+                    epochs=100, validation_data = validation_generator,
                     validation_steps = validation_generator.samples // train_generator.batch_size)
