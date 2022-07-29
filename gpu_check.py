89b7bc948a75235400b46cf10fe14a222fb70c01
Ofek Ophir <ophir.ofek@gmail.com>
2022-07-29 23:49:54 +0300

    removed un used piece of code

--- gpu_check.py
+++ gpu_check.py
@@ -1,10 +1,6 @@
 import tensorflow as tf
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
-
-if tf.test.gpu_device_name():
-    print('Default GPU Device:.{}'.format(tf.test.gpu_device_name()))
 
 
 def get_device() -> str:
