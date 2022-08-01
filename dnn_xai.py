2cb9fb5cede6982691d1d9f63d16fe21392d4353
Ofek Ophir <ophir.ofek@gmail.com>
2022-08-02 00:19:42 +0300

    fixed class architecture

--- dnn_xai.py
+++ dnn_xai.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import numpy as np
 from dnn import DNNClassifier
 from gpu_check import get_device
 import shap
@@ -21,38 +22,62 @@ class Explainer:
     """
     Explainer uses Shapley Values in order to explain feature importance in a model.
     """
-    def __init__(self, title: str = None) -> None:
+    def __init__(self, title: str = None, data: pd.DataFrame = None) -> None:
         """
         :param title: name of the model.
+        :param data: type of data to be processed.
         """
         self.model = tf.keras.models.load_model(filepath=model_path)
         self.title = title
+        self.data = data
 
-    def explain_model(self, size: tuple = (10, 10)) -> None:
+    def explain_model(self) -> None:
         """
-        :param size: size of the summary plot.
         :return: shap.summary_plot for a multi-output model.
         """
-        dummy = DNNClassifier(db=data_mouse, n_layers=0, weight_decay=0, dense_size=[], activation_function=[],
+        # use a dummy classifier to preprocess the data
+        dummy = DNNClassifier(db=self.data, n_layers=0, weight_decay=0, dense_size=[], activation_function=[],
                               learning_rate=0, drop_rate=[], batch_size=0, n_epochs=0, optimizer='adam')
-        data = dummy.preprocess_data(data_mouse)
+        data = dummy.preprocess_data(self.data)
 
+        # get features' names
         features = list(data.columns)
         features.remove('dendrite_type')
 
+        # split into train, val and test
         x_train, y_train, x_val, y_val, x_test, y_test = dummy.split_train_val_test(data)
 
+        # use the kernel explainer to get the Shapley values
         kernel_explainer = shap.KernelExplainer(self.model, x_test)
-        shapley_vals = kernel_explainer.shap_values(x_test)
-        shap.summary_plot(shap_values=shapley_vals, features=x_test, feature_names=features, plot_size=size,
-                          color=plt.get_cmap("Pastel1"), title=self.title, class_names=["aspiny", "spiny"])
+        shapley_values = kernel_explainer.shap_values(x_test)
+
+        # draw summary plot
+        self._draw_summary_plot(shapley_values, x_test, features)
+
+    def _draw_summary_plot(self, shapley_values: list, data: np.ndarray,
+                           features: list, size: tuple = (10, 10)) -> None:
+        """
+        :param shapley_values: shap values obtained from the explainer.
+        :param data: data that was tested.
+        :param features: features' names
+        :param size: size of the plot
+        """
+        plt.figure()
+        shap.summary_plot(shap_values=shapley_values, features=data, feature_names=features, plot_size=size,
+                          show=False, color=plt.get_cmap("Pastel1"), class_names=["aspiny", "spiny"])
+        plt.title(self.title)
+        plt.tight_layout()
+        plt.draw()
 
 
 def main():
-    models = ['Mouse Feature Importance', 'Human Feature Importance']
-    for model in models:
-        xai = Explainer(title=model)
+    models = ['Human Feature Importance', 'Mouse Feature Importance']
+    data = [data_human, data_mouse]
+    for idx, val in enumerate(models):
+        xai = Explainer(title=val, data=data[idx])
         xai.explain_model()
+
+    plt.show()
 
 
 if __name__ == '__main__':
