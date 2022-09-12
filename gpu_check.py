2f0bced974ba638826e95ac2cc944effcd7ed1f3
Ofek Ophir <ophir.ofek@gmail.com>
2022-09-12 16:14:52 +0300

    added docstring to get_device function:

--- gpu_check.py
+++ gpu_check.py
@@ -1,9 +1,15 @@
 import tensorflow as tf
 import os
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 
 def get_device() -> str:
+    """
+    prints out your physical devices.
+    The user determines which device to use GPU/CPU by input.
+    :return: device to be used.
+    """
     print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
     gpus = tf.config.list_physical_devices('GPU')
     for gpu in gpus:
