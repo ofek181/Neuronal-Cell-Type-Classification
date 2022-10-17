fbaab0f38557469de580c6d2988365b3f5c8aa40
Ofek Ophir <ophir.ofek@gmail.com>
2022-10-17 16:26:12 +0300

    added multiclass classification helpers

--- helper_functions.py
+++ helper_functions.py
@@ -14,3 +14,17 @@ def calculate_metrics(y_true: np.ndarray, y_pred: np.ndarray) -> tuple:
     recall = recall_score(y_true, y_pred)
     roc_auc = roc_auc_score(y_true, y_pred)
     return accuracy, f1, precision, recall, roc_auc
+
+def calculate_metrics_multiclass(y_true: np.ndarray, y_pred: np.ndarray, y_pred_proba) -> tuple:
+    """
+    :param y_true: true labels provided by the dataset.
+    :param y_pred: predicted labels provided by the model.
+    :param y_pred_proba: predicted probabilities for labels.
+    :return: accuracy, f1 score, precision, recall and roc auc score.
+    """
+    accuracy = accuracy_score(y_true, y_pred)
+    f1 = f1_score(y_true, y_pred, average='micro')
+    precision = precision_score(y_true, y_pred, average='micro')
+    recall = recall_score(y_true, y_pred, average='micro')
+    roc_auc = roc_auc_score(y_true, y_pred_proba, multi_class='ovr')
+    return accuracy, f1, precision, recall, roc_auc
