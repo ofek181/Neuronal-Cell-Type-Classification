cabf0bcb753adca3882e82225d39189bf8645c96
Ofek <ophir.ofek@gmail.com>
2023-03-24 15:05:18 +0300

    1d conv network is working on time series data

--- 1dconv_time_series.py
+++ 1dconv_time_series.py
@@ -3,8 +3,10 @@ import numpy as np
 import tensorflow as tf
 from glob import glob
 from scipy import signal
-from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Concatenate, Conv1D, Dense, Flatten, Input, MaxPooling1D, Reshape
+from tensorflow.keras.models import Model
+from sklearn.model_selection import train_test_split
+from keras.utils.np_utils import to_categorical
 
 from gpu_check import get_device
 import warnings
@@ -18,7 +20,45 @@ file_path = os.path.dirname(os.path.realpath(__file__))
 data_path = file_path + '/data/single_spike/mouse/'
 
 
+def create_model(kernel_size: int = 3, activation: str = 'relu', padding: str = 'valid'):
+    # input layer
+    inp = Input(shape=(150, 1))
+
+    # conv layer 1
+    x = Conv1D(filters=32, kernel_size=kernel_size, activation=activation, padding=padding)(inp)
+
+    # max pooling 1
+    x = MaxPooling1D(2)(x)
+
+    # conv layer 2
+    x = Conv1D(filters=32, kernel_size=kernel_size, activation=activation, padding=padding)(x)
+
+    # max pooling 2
+    x = MaxPooling1D(2)(x)
+
+    # conv layer 3
+    x = Conv1D(filters=16, kernel_size=kernel_size, activation=activation, padding=padding)(x)
+
+    # max pooling 3
+    x = MaxPooling1D(2)(x)
+
+    # flatten layer
+    x = Flatten()(x)
+
+    # fully connected layer 1
+    x = Dense(100, activation=activation)(x)
+
+    # fully connected layer 2
+    x = Dense(50, activation=activation)(x)
+
+    # output layer
+    out = Dense(5, activation="softmax")(x)
+
+    return Model(inputs=inp, outputs=out)
+
+
 def main():
+    # read the data
     directories = ['glutamatergic', 'htr3a', 'pvalb', 'sst', 'vip']
     features = []
     labels = []
@@ -31,23 +71,31 @@ def main():
 
             features.append(array)
             labels.append(idx)
+
+    # convert to a tensor
     features = np.array(features)
-    labels = np.array(labels)
-    features = tf.convert_to_tensor(features, dtype=float)
-    labels = tf.convert_to_tensor(tf.one_hot(labels, 5))
+    labels = to_categorical(np.array(labels), num_classes=5)
+    # features = tf.convert_to_tensor(features, dtype=float)
+    # labels = tf.convert_to_tensor(tf.one_hot(labels, 5))
+
+    # split into train, validation and test
+    x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42, shuffle=True)
 
     # define the model
-    model = Sequential()
-    model.add(Dense(150, input_shape=(150,)))
-    model.add(Dense(50, input_shape=(150,)))
-    model.add(Dense(20, input_shape=(150,)))
-    model.add(Dense(5))
+    model = create_model()
 
     # compile the model
-    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3), loss='categorical_crossentropy')
+    model.compile(optimizer='adam',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
 
     # fit the model
-    history = model.fit(features, labels, epochs=10, validation_split=0.2, shuffle=True)
+    history = model.fit(x=x_train,
+                        y=y_train,
+                        batch_size=8,
+                        epochs=10,
+                        validation_data=(x_test, y_test),
+                        shuffle=True)
 
 
 if __name__ == '__main__':
